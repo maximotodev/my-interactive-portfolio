479da52c94b955ed165e4759dae3266eeff50dae
maximotodev <maximotodev@proton.me>
2025-07-15 00:04:35 -0400

    fix: Configure WhiteNoise to serve media files in production

--- backend/portfolio_project/settings.py
+++ backend/portfolio_project/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedRootStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
