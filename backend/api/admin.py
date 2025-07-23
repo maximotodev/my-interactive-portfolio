42c1f2e5ed245f2802744c3d67ecb7e29cbb45b6
maximotodev <maximotodev@proton.me>
2025-07-23 00:45:33 -0400

    feat: Register Post model with Django admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,8 @@
 # backend/api/admin.py
 
 from django.contrib import admin
-from .models import Project, Certification, Post # Import your models
+# --- Import all three of your models ---
+from .models import Project, Certification, Post 
 
 # Register your models here.
 
@@ -21,10 +22,14 @@ class CertificationAdmin(admin.ModelAdmin):
     list_display = ('name', 'issuing_organization', 'date_issued')
     list_filter = ('issuing_organization', 'date_issued')
 
+# --- ADD THIS NEW SECTION FOR THE BLOG POSTS ---
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    """
+    Customizes the display of the Post model in the admin panel.
+    """
     list_display = ('title', 'slug', 'published_date', 'is_published')
     list_filter = ('is_published', 'published_date')
     search_fields = ('title', 'content')
-    # This will automatically pre-populate the slug field from the title
+    # This will automatically pre-populate the slug field from the title as you type
     prepopulated_fields = {'slug': ('title',)}
